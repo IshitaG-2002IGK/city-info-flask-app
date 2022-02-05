03f8ebc722e8770c7511338a68c533dc09b8ecc6
Ishita <ishitagops12@gmail.com>
2022-02-05 20:39:04 +0530

    city info flask app updated

---
+++ scapanza.py
@@ -0,0 +1,52 @@
+'''
+Created on 
+Course work: 
+@author: Ishita
+Source:
+    
+'''
+# with path variable return a dict consists of 
+# state and population of city supplied through parameter
+
+
+# # Import necessary modules
+
+from flask import Flask, render_template, jsonify
+import random, json
+
+app = Flask(__name__)
+
+@app.route('/')
+def start():
+
+    index = random.randrange(0,2)
+    city = [{
+        "city"   : "Chennai",
+        "state"  : "Tamilnadu",
+        "population": "7.8 million"    
+    },
+    {
+        "city"   : "Toronto",
+        "state"  : "Ontario",
+        "population" : "6.8 million"
+    }
+]
+
+
+    # return render_template('index.html')
+    return jsonify(city[index])
+
+@app.route('/city/<city>')
+def city_pop(city):
+
+    f = open("info.json")
+    json_data = json.load(f)
+    for cities in json_data['city_info'] :
+        
+        if cities["city"] == city :
+            return jsonify([cities])
+
+
+
+if __name__== "__main__":
+    app.run(host="0.0.0.0", debug = True)
